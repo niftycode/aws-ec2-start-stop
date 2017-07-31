6fae74f22374474bc89f3d0322e184149ff93ee0
Bodo Schönfeld <bodo.schoenfeld@niftycode.de>
2017-07-31 10:16:49 +0200

    added unit test

---
+++ tests.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+import unittest
+import start_stop_ec2
+
+
+class MyTests(unittest.TestCase):
+    def testReadFile(self):
+
+
+if __name__ == "__main__":
+    unittest.main()
